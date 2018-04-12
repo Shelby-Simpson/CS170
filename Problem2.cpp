180f6889f448a00d0aefc010e3d1ed5fa9305c49
Shelby-Simpson <37957895+Shelby-Simpson@users.noreply.github.com>
2018-04-11 20:13:29 -0400

    Add files via upload

--- Problem2.cpp
+++ Problem2.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 void plot(double num);
 int sumSequence(vector<double> &vec);
+int sumSequence2(vector<double> &vec);
 
 int main(){
 	vector<double> sequence;
@@ -12,14 +13,19 @@ int main(){
 	for (int i = 0; i >= 0; i++){
 		cout << "Please enter integer " << i + 1 << " in the sequence: \n(Enter anything other than an integer to end)\n" << endl;
 		cin >> temp;
-		if (cin.fail()) break;
+		if (cin.fail()){
+			cin.clear();
+			cin.ignore(10000, '\n');
+			break;
+		}
 		else sequence.push_back(temp);
 	}
 	plot(sumSequence(sequence));
 	system("pause");
 }
 
-int sumSequence(vector<double> &vec){
+int sumSequence(vector<double> &vec)
+{
 	int sum = 0;
 	double size = vec.size() - 1;
 	for (int i = 0; i <= (int) size; i++){
@@ -27,10 +33,16 @@ int sumSequence(vector<double> &vec){
 	}
 	return sum;
 }
-void plot(double num){
+void plot(double num)
+{
 	double numAst = num * 10;
 	for (int i = 1; i <= (int) numAst; i++){
 		cout << "*";
 	}
 	return;
+}
+
+int sumSequence2(vector<int> &vec)
+{
+	
 }
