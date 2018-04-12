d1bfe190f98910a5205f8d8befec9641fe77f5c9
Shelby-Simpson <37957895+Shelby-Simpson@users.noreply.github.com>
2018-04-12 14:56:55 -0400

    Add files via upload

--- Problem2.cpp
+++ Problem2.cpp
@@ -3,46 +3,78 @@
 
 using namespace std;
 
-void plot(double num);
-int sumSequence(vector<double> &vec);
-int sumSequence2(vector<double> &vec);
+void plot(int (*sumSequence)(int), int min, int max);
+int sumSequence1(int num);
+int sumSequence2(int num);
 
 int main(){
-	vector<double> sequence;
-	double temp;
-	for (int i = 0; i >= 0; i++){
-		cout << "Please enter integer " << i + 1 << " in the sequence: \n(Enter anything other than an integer to end)\n" << endl;
-		cin >> temp;
-		if (cin.fail()){
-			cin.clear();
-			cin.ignore(10000, '\n');
-			break;
-		}
-		else sequence.push_back(temp);
+	int countMin, countMax;
+	cout << "Please enter the minimum integer value of count: \n(Must be between 1 and 10)" << endl;
+	cin >> countMin;
+	while (cin.fail() || countMin < 1 || countMin > 10)
+	{
+		cin.clear();
+		cin.ignore(10000, '\n');
+		cout << "You did not enter an acceptable value.  Please try again:" << endl;
+		cin >> countMin;
 	}
-	plot(sumSequence(sequence));
+	cout << "please enter the maximum integer value of count: \n(Must be between 1 and 10 and greater than or equal to min" << endl;
+	cin >> countMax;
+	while (cin.fail() || countMax < 1 || countMax > 10 || countMax < countMin)
+	{
+		cin.clear();
+		cin.ignore(10000, '\n');
+		cout << "You did not enter an acceptable value.  Please try again:" << endl;
+		cin >> countMax;
+	}
+	cout << endl;
+	cout << "The following sums are calculated using iteration:" << endl;
+	plot(&sumSequence1, countMin, countMax);
+	cout << "The following sums are calculated using recursion:" << endl;
+	plot(&sumSequence2, countMin, countMax);
 	system("pause");
 }
 
-int sumSequence(vector<double> &vec)
+int sumSequence1(int num)
 {
 	int sum = 0;
-	double size = vec.size() - 1;
-	for (int i = 0; i <= (int) size; i++){
-		sum = sum + (int) vec[i];
+	int numArray[10] = { 1, 2, -2, 4, 5, -1, 7, -7, 9, 10 };
+	cout << "This is the sequence of numbers being summed together:" << endl;
+	for (int i = 0; i < num; i++)
+	{
+		sum += numArray[i];
+		cout << numArray[i] << ",";
 	}
+	cout << endl;
 	return sum;
 }
-void plot(double num)
+
+int sumSequence2(int num)
 {
-	double numAst = num * 10;
-	for (int i = 1; i <= (int) numAst; i++){
-		cout << "*";
+	
+	int numArray[10] = { 1, 2, -2, 4, 5, -1, 7, -7, 9, 10 };
+	if (num <= 1)
+	{
+		return numArray[0];
 	}
+	else {
+		return numArray[num - 1] + sumSequence2(num - 1);
+	}
+}
+
+void plot(int (*sumSequence)(int), int min, int max)
+{
+	for (int i = min; i <= max; i++)
+	{
+		cout << '\n';
+		double numAst = (*sumSequence)(i) * 10;
+		cout << "The sum of this iteration is " << numAst / 10 << endl;
+		for (int i = 1; i <= (int)numAst; i++){
+			cout << "*";
+		}
+		cout << '\n' << endl;
+	}
+	
 	return;
 }
 
-int sumSequence2(vector<int> &vec)
-{
-	
-}
