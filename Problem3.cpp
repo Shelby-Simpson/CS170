67b7361e6208e47661d2023bd94bfc26f6fc55ce
Shelby-Simpson <37957895+Shelby-Simpson@users.noreply.github.com>
2018-04-13 23:04:28 -0400

    Add files via upload

--- Problem3.cpp
+++ Problem3.cpp
@@ -4,21 +4,58 @@
 using namespace std;
 
 void reverseDisplay1(int value);
-int reverse(int value);
+void reverseDisplay2(int value);
 
 int main()
 {
-	int num = 5678;
+	int num;
+	cout << "Please enter a positive integer that you would like reversed:" << endl;
+	cin >> num;
+	while (cin.fail() || num < 0)
+	{
+		cout << "Please try again:" << endl;
+		cin >> num;
+	}
 	reverseDisplay1(num);
+	reverseDisplay2(num);
 	system("pause");
 }
 
 void reverseDisplay1(int value)
 {
-	cout << reverse(value) << endl;
+	if (value < 10) cout << value << endl;
+	else
+	{
+		int temp;		
+		temp = value % 10;
+		cout << temp;
+		reverseDisplay1(value / 10);
+	}
 }
 
-int reverse(int value)
+void reverseDisplay2(int value)
 {
-
+	if (value < 10) cout << value;
+	else
+	{
+		int temp = value;
+		int count = 0;
+		int newValue = value;
+		while (newValue > 10)
+		{
+			newValue = newValue / 10;
+			count++;
+		}
+		int power = newValue * pow(10, count);
+		int powerCheck = (newValue - 1) * pow(10, count);
+		if (value % (power*10) < power - powerCheck)
+		{
+			reverseDisplay2(value % power);
+			cout << 0;
+		}
+		else {
+			reverseDisplay2(value % power);
+			cout << newValue;
+		}
+	}
 }
