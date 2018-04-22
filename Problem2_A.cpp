e38f25d03e2ad74f19a7f780c24b44221eadfe4f
Shelby-Simpson <37957895+Shelby-Simpson@users.noreply.github.com>
2018-04-22 17:27:36 -0400

    Add files via upload

--- Problem2_A.cpp
+++ Problem2_A.cpp
@@ -6,18 +6,24 @@ using namespace std;
 
 int main()
 {
-	string result;
-	string line;
+	char ** arr = new char *[35];
+	cout << "Please enter a line of text at most 70 characters long (enter an empty line to end):" << endl;
+	cin.getline(arr[35], 70);
+	for (int i = 0; i < 35; i++)
+	{
+		for (int j = 0; j < 70; j++)
+		{
+			if ()
+		}
+	}
 	
 	cout << "Please enter several lines of text.  This program will sort the words into alphabetical order." << endl;
 	while (line.length() != 0){
 		cout << "Please enter a line of text (Enter empty line to end):" << endl;
-		getline(cin, line);
+		getline(arr, line);
 		result += line;
 	}
-	int arrSize = result.length() / 2 + 1;
-	char ** arr = new char *[arrSize];
-	vector<char> temp;
+	
 	for (int i = 0; i < result.length(); i++)
 	{
 		for (int j = 0; j < result.length(); j++)
