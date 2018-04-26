0babdbfa783d3750f0ad804fbb4db00d0ac97a8e
Shelby-Simpson <37957895+Shelby-Simpson@users.noreply.github.com>
2018-04-26 16:40:13 -0400

    Add files via upload

--- Problem2_A.cpp
+++ Problem2_A.cpp
@@ -1,34 +1,108 @@
 #include <iostream>
-#include <string>
-#include <vector>
 
 using namespace std;
 
 int main()
 {
-	char ** arr = new char *[35];
-	cout << "Please enter a line of text at most 70 characters long (enter an empty line to end):" << endl;
-	cin.getline(arr[35], 70);
-	for (int i = 0; i < 35; i++)
+	char string1[30];
+	char string2[30];
+	char string3[30];
+	char *arr[45];
+	int count = 0;
+	bool spaceTest = false;
+	cout << "Please enter 3 lines of text that are at most 30 characters long each(^Z to end early...noob):" << endl;
+	cin.getline(string1,30);
+	cin.getline(string2,30);
+	cin.getline(string3,30);
+	for (int i = 0; i < 30; i++)
 	{
-		for (int j = 0; j < 70; j++)
+		if (isspace(string1[i]))
 		{
-			if ()
+			spaceTest = true;
+			continue;
+		}
+		else
+		{
+			if (spaceTest)
+			{
+				*arr[count] = string1[i];
+				count++;
+				spaceTest = false;
+			}
+
 		}
 	}
-	
-	cout << "Please enter several lines of text.  This program will sort the words into alphabetical order." << endl;
-	while (line.length() != 0){
-		cout << "Please enter a line of text (Enter empty line to end):" << endl;
-		getline(arr, line);
-		result += line;
-	}
-	
-	for (int i = 0; i < result.length(); i++)
+	for (int i = 0; i < 30; i++)
 	{
-		for (int j = 0; j < result.length(); j++)
+		if (isspace(string2[i]))
 		{
-			
+			spaceTest = true;
+			continue;
+		}
+		else
+		{
+			if (spaceTest)
+			{
+				*arr[count] = string2[i];
+				count++;
+				spaceTest = false;
+			}
+
 		}
 	}
+	for (int i = 0; i < 30; i++)
+	{
+		if (isspace(string3[i]))
+		{
+			spaceTest = true;
+			continue;
+		}
+		else
+		{
+			if (spaceTest)
+			{
+				*arr[count] = string3[i];
+				count++;
+				spaceTest = false;
+			}
+
+		}
+	}
+
+	system("pause");
+
+	/*
+	int lineCount = 0;
+	bool spaceTest = true;
+	char ** arr = new char *[45];
+	cout << "Please enter 3 lines of text that are at most 30 characters long each(^Z to end early...noob):" << endl;
+	
+	while (true)
+	{
+		cin.getline(arr[43], 30);
+		if (cin.eof()) break;
+		cin.getline(arr[44], 30);
+		if (cin.eof()) break;
+		cin.getline(arr[45], 30);
+	}
+	for (int i = 0; i < 45; i++)
+	{
+		for (int j = 0; j < 30; j++)
+		{
+			if (isspace(j))
+			{
+				spaceTest = true;
+				break;
+			}
+			else
+			{
+				spaceTest = false;
+				arr[i][count] = j;
+			}
+			if (!spaceTest) count++;
+		}
+	}*/
 }
+
+
+
